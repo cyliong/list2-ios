7c7e665cde1df5db1f2ef14ca4c0f5690f85cbf9
cyliong <liongtheprogrammer@gmail.com>
2020-10-05 23:34:01 +0800

    Add the Constants file to project

---
+++ List2/Constants.swift
@@ -0,0 +1,6 @@
+enum Constants {
+    static let appTitle = "List"
+    static let widgetKind = "com.example.ltp.list-widget"
+    static let appGroupID = "group.com.example.ltp.list"
+    static let noItems = "No Items"
+}
