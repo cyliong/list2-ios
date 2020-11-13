85319536318a67c8550ed3a6f66e3e5eea42f350
cyliong <liongtheprogrammer@gmail.com>
2020-11-13 15:44:56 +0800

    Define a deep link constant for widget to launch the Edit Item screen

--- List2/Constants.swift
+++ List2/Constants.swift
@@ -3,4 +3,5 @@ enum Constants {
     static let widgetKind = "com.example.ltp.list-widget"
     static let appGroupID = "group.com.example.ltp.list"
     static let noItems = "No Items"
+    static let editItemURLString = "list:///item/edit/"
 }
