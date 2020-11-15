f9a94efac3e0617693d169e5bf0267fd163648c7
cyliong <liongtheprogrammer@gmail.com>
2020-11-15 21:04:12 +0800

    Define a deep link constant for widget to launch the New Item screen

--- List2/Constants.swift
+++ List2/Constants.swift
@@ -3,5 +3,6 @@ enum Constants {
     static let widgetKind = "com.example.ltp.list-widget"
     static let appGroupID = "group.com.example.ltp.list"
     static let noItems = "No Items"
+    static let addItemURLString = "list:///item/add"
     static let editItemURLString = "list:///item/edit/"
 }
