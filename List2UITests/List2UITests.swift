b76445b7b4a4d0637121a45688fe9c254a8c0fd5
cyliong <liongtheprogrammer@gmail.com>
2021-04-13 21:10:16 +0800

    Implement sample UI test

--- List2UITests/List2UITests.swift
+++ List2UITests/List2UITests.swift
@@ -12,7 +12,17 @@ class List2UITests: XCTestCase {
     }
     
     func test_Add_an_item_to_the_list() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        app.navigationBars["List"].buttons["plus"].tap()
         
+        let title = app.textFields["Enter an item"]
+        title.tap()
+        title.typeText("Item 1")
+        app.navigationBars["New Item"].buttons["Save"].tap()
+        
+        XCTAssertTrue(app.buttons["Item 1"].exists)
     }
     
 }
