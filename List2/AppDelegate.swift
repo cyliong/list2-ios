2b422abcaec7648f7f4174db6731057d63ba8ae9
cyliong <liongtheprogrammer@gmail.com>
2022-04-07 21:25:51 +0800

    Migrate app initialization code from AppDelegate to ListApp

--- List2/AppDelegate.swift
+++ List2/AppDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return ListDatabase.shared.create()
+        return true
     }
 
     // MARK: UISceneSession Lifecycle
--- List2/ListApp.swift
+++ List2/ListApp.swift
@@ -4,6 +4,10 @@ import SwiftUI
 struct ListApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     
+    init() {
+        _ = ListDatabase.shared.create()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
