5d467913840df87f96fe94ea76d8c2bf05ec83eb
cyliong <liongtheprogrammer@gmail.com>
2022-04-08 20:31:02 +0800

    Refactor UI test to the Given-When-Then format

--- List2UITests/List2UITests.swift
+++ List2UITests/List2UITests.swift
@@ -14,13 +14,17 @@ class List2UITests: XCTestCase {
     func test_Add_an_item_to_the_list() throws {
         let newItem = "Item 1"
         
-        app.buttons["Add"].tap()
+        // Given I am on the list screen
+        XCTAssertTrue(app.navigationBars["List"].exists)
         
+        // When I add a new item
+        app.buttons["Add"].tap()
         let title = app.textFields["title"]
         title.tap()
         title.typeText(newItem)
         app.buttons["Save"].tap()
         
+        // Then I should see the new item on the list
         XCTAssertTrue(app.buttons[newItem].waitForExistence(timeout: 1))
     }
     
